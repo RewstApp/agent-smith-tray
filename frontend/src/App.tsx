032f58592a5e47cd9a989dcb1e24ed6ccbdd466b
mlataza <1750947+mlataza@users.noreply.github.com>
2025-08-11 21:49:28 +0800

    fix: show agent smith dashboard on show of tray icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,25 +15,24 @@ function App() {
     Greet(name).then(updateResultText);
   }
 
+  const dashboardUrl = "https://pedroaviary-agent-smith.rew.st/s/home";
+
   return (
-    <div id="App">
-      <img src={logo} id="logo" alt="logo" />
-      <div id="result" className="result">
-        {resultText}
-      </div>
-      <div id="input" className="input-box">
-        <input
-          id="name"
-          className="input"
-          onChange={updateName}
-          autoComplete="off"
-          name="input"
-          type="text"
-        />
-        <button className="btn" onClick={greet}>
-          Greet
-        </button>
-      </div>
+    <div
+      style={{
+        overflow: "hidden",
+      }}
+    >
+      <iframe
+        src={dashboardUrl}
+        style={{
+          width: "100%",
+          height: "100vh",
+          overflow: "hidden",
+          padding: 0,
+          margin: 0,
+        }}
+      />
     </div>
   );
 }
