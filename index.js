83e7290ec86c6f261516690c3a04214e7a054a5d
mlataza <1750947+mlataza@users.noreply.github.com>
2025-09-15 21:40:16 +0800

    fix: add tray context menu

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Tray, Menu } = require("electron");
+const { nativeImage } = require("electron/common");
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -9,6 +10,30 @@ const createWindow = () => {
   win.loadFile("index.html");
 };
 
+// save a reference to the Tray object globally to avoid garbage collection
+let tray;
+
+const offlineIcon = nativeImage.createFromPath(__dirname + "/icon/offline.png");
+const onlineIcon = nativeImage.createFromPath(__dirname + "/icon/online.png");
+
 app.whenReady().then(() => {
-  createWindow();
+  tray = new Tray(offlineIcon);
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Show",
+      click: () => {
+        createWindow();
+      },
+    },
+    { role: "quit" },
+  ]);
+
+  tray.setContextMenu(contextMenu);
+  tray.setTitle("Agent Smith");
+  tray.setToolTip("Offline");
+});
+
+app.on("window-all-closed", () => {
+  // having this listener active will prevent the app from quitting.
 });
